a911e248c50a38d73d88cdf8e6823a1615c2d921
Ahmadreza Hamid Fard <61118112+AhmadrezaHamidi@users.noreply.github.com>
2024-05-28 15:37:57 +0330

    changeto9version

--- samples/UnitOfWork.Host/Program.cs
+++ samples/UnitOfWork.Host/Program.cs
@@ -10,6 +10,7 @@ namespace Arch.EntityFrameworkCore.UnitOfWork.Host
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
